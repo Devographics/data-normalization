257d11ed89fe8b19dcc9598a0be932f66baf0a64
Raphaël Benitte <501642+plouc@users.noreply.github.com>
2019-12-10 07:28:01 +0900

    init

---
+++ cli.mjs
@@ -0,0 +1,206 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import program from 'commander'
+import {
+    extractTypeformConfig,
+    fetchTypeformResponses,
+    normalizeTypeformResponses,
+} from './lib/typeform/index.mjs'
+import {
+    recreateIndex,
+    indexSurveyResponses,
+} from './lib/elastic/index.mjs'
+import { anonymizeResponses } from './lib/anonymize.mjs'
+import  {
+    cleanupMongoResponses,
+    saveResponsesToMongo,
+    fetchMongoResponses,
+    normalizeMongoResponses,
+} from './lib/mongo/index.mjs'
+import surveys from './conf/surveys.mjs'
+
+const getSurveyConfig = (survey, year) => {
+    const surveyConfig = surveys.find(s => {
+        return s.survey === survey && s.year === Number(year)
+    })
+    if (!surveyConfig) {
+        console.log(`no config found matching survey: ${survey}, for year: ${year}`)
+        process.exit(1)
+    }
+
+    return surveyConfig
+}
+
+//
+// TYPEFORM
+//
+program
+    .command('tf-config <survey> <year>')
+    .description('prepare config for a typeform survey and store it in yaml')
+    .action(async (survey, year) => {
+        const surveyConfig = getSurveyConfig(survey, year)
+        if (surveyConfig.source === 'typeform') {
+            await extractTypeformConfig(surveyConfig)
+        } else {
+            console.log(`survey ${surveyConfig.survey} ${surveyConfig.year} source is not typeform (found: ${surveyConfig.source})`)
+            process.exit(1)
+        }
+    })
+
+program
+    .command('tf-fetch <survey> <year>')
+    .description('fetch raw responses from typeform and store them locally in ndjson')
+    .action(async (survey, year) => {
+        const surveyConfig = getSurveyConfig(survey, year)
+        if (surveyConfig.source === 'typeform') {
+            await fetchTypeformResponses(surveyConfig)
+        } else {
+            console.log(`survey ${surveyConfig.survey} ${surveyConfig.year} source is not typeform (found: ${surveyConfig.source})`)
+            process.exit(1)
+        }
+    })
+
+program
+    .command('tf-norm <survey> <year>')
+    .description('normalize typeform raw responses and store the result locally in ndjson')
+    .action(async (survey, year) => {
+        const surveyConfig = getSurveyConfig(survey, year)
+        if (surveyConfig.source === 'typeform') {
+            await normalizeTypeformResponses(surveyConfig)
+        } else {
+            console.log(`survey ${surveyConfig.survey} ${surveyConfig.year} source is not typeform (found: ${surveyConfig.source})`)
+            process.exit(1)
+        }
+    })
+
+//
+// ELASTIC
+//
+program
+    .command('es-setup')
+    .description('setup elasticsearch, recreate indices, this operation will remove existing indices')
+    .action(async () => {
+        await recreateIndex()
+    })
+
+program
+    .command('es-index <survey> <year>')
+    .description('index survey normalized responses in elasticsearch')
+    .action(async (survey, year) => {
+        const surveyConfig = getSurveyConfig(survey, year)
+        await indexSurveyResponses(surveyConfig)
+    })
+
+//
+// MONGO
+//
+program
+    .command('mongo-save <survey> <year>')
+    .description('save normalized responses to mongo')
+    .action(async (survey, year) => {
+        const surveyConfig = getSurveyConfig(survey, year)
+        await saveResponsesToMongo(surveyConfig)
+    })
+
+program
+    .command('mongo-fetch <survey> <year>')
+    .description('fetch non normalized mongo responses, and store them locally in ndjson')
+    .action(async (survey, year) => {
+        const surveyConfig = getSurveyConfig(survey, year)
+        if (surveyConfig.source === 'mongo') {
+            await fetchMongoResponses(surveyConfig)
+        } else {
+            console.log(`survey ${surveyConfig.survey} ${surveyConfig.year} source is not mongo (found: ${surveyConfig.source})`)
+            process.exit(1)
+        }
+    })
+
+program
+    .command('mongo-reset')
+    .description('reset normalized mongo collection')
+    .action(async () => {
+        await cleanupMongoResponses()
+    })
+
+program
+    .command('mongo-norm <survey> <year>')
+    .description('normalize mongo responses, and store them locally in ndjson')
+    .action(async (survey, year) => {
+        const surveyConfig = getSurveyConfig(survey, year)
+        if (surveyConfig.source === 'mongo') {
+            await normalizeMongoResponses(surveyConfig)
+        } else {
+            console.log(`survey ${surveyConfig.survey} ${surveyConfig.year} source is not mongo (found: ${surveyConfig.source})`)
+            process.exit(1)
+        }
+    })
+
+//
+// UTILS
+//
+program
+    .command('anon <survey> <year>')
+    .description('make normalized responses anonymous, and store the result locally in ndjson')
+    .action(async (survey, year) => {
+        const surveyConfig = getSurveyConfig(survey, year)
+        await anonymizeResponses(surveyConfig)
+    })
+
+program
+    .command('transform-load')
+    .description('transform and load data for all js surveys')
+    .action(async () => {
+        const jsSurveys = surveys.filter(s => s.survey === 'js')
+
+        const tfSurveys = jsSurveys.filter(s => s.source === 'typeform')
+        const mongoSurveys = jsSurveys.filter(s => s.source === 'mongo')
+
+        console.log('—————————————————————————————————————————————————')
+        console.log(' extract typeform configs')
+        console.log('—————————————————————————————————————————————————')
+        for (const survey of tfSurveys) {
+            await extractTypeformConfig(survey)
+        }
+
+        console.log('—————————————————————————————————————————————————')
+        console.log(' normalize typeform based surveys')
+        console.log('—————————————————————————————————————————————————')
+        for (const survey of tfSurveys) {
+            await normalizeTypeformResponses(survey)
+        }
+
+        console.log('—————————————————————————————————————————————————')
+        console.log(' re-fetch mongo surveys raw responses')
+        console.log('—————————————————————————————————————————————————')
+        for (const survey of mongoSurveys) {
+            await fetchMongoResponses(survey)
+        }
+
+        console.log('—————————————————————————————————————————————————')
+        console.log(' normalize mongo based surveys')
+        console.log('—————————————————————————————————————————————————')
+        for (const survey of mongoSurveys) {
+            await normalizeMongoResponses(survey)
+        }
+
+        console.log('—————————————————————————————————————————————————')
+        console.log(' empty normalized responses mongo collection')
+        console.log('—————————————————————————————————————————————————')
+        await cleanupMongoResponses()
+
+        console.log('—————————————————————————————————————————————————')
+        console.log(' save normalized responses in mongo')
+        console.log('—————————————————————————————————————————————————')
+        for (const survey of jsSurveys) {
+            await saveResponsesToMongo(survey)
+        }
+
+        console.log('—————————————————————————————————————————————————')
+        console.log(' generate anonymized responses')
+        console.log('—————————————————————————————————————————————————')
+        for (const survey of jsSurveys) {
+            await anonymizeResponses(survey)
+        }
+    })
+
+program.parse(process.argv)
---
+++ conf/surveys.mjs
@@ -0,0 +1,302 @@
+import { sections, experience } from '../lib/constants.mjs'
+
+export default [
+    {
+        survey: 'js',
+        year: 2016,
+        source: 'typeform',
+        typeformId: 'Mulmxw',
+        sections: {
+            [sections.JAVASCRIPT_FLAVORS]: {
+                title: 'JavaScript Flavor'
+            },
+            [sections.FRONTEND_FRAMEWORKS]: {
+                title: 'Front-End',
+                freeform: 'Other Front-End Frameworks'
+            },
+            [sections.STATE_MANAGEMENT]: {
+                title: 'State Management',
+                freeform: 'Other State Management Libraries'
+            },
+            [sections.DATA_LAYER]: {
+                title: 'API Layer',
+                freeform: 'Other API layer solutions'
+            },
+            [sections.BACKEND_FRAMEWORKS]: {
+                title: 'Stacks',
+                freeform: 'Other stacks'
+            },
+            [sections.TESTING]: {
+                title: 'Testing',
+                freeform: 'Other testing frameworks'
+            },
+            [sections.CSS]: {
+                title: 'CSS',
+                freeform: 'Other CSS solutions'
+            },
+            [sections.BUILD_TOOLS]: {
+                title: 'Build Tools',
+                freeform: 'Other build tools'
+            },
+            [sections.MOBILE_DESKTOP]: {
+                title: 'Mobile',
+                freeform: 'Other mobile apps solutions'
+            }
+        },
+        experience: {
+            [`I've used it before, and would use it again`]: experience.WOULD_USE,
+            [`I've used it before, and would not use it again`]: experience.WOULD_NOT_USE,
+            [`I've heard of it, and would like to learn it`]: experience.INTERESTED,
+            [`I've heard of it, and am not interested`]: experience.NOT_INTERESTED,
+            [`I've never heard of it`]: experience.NEVER_HEARD
+        },
+    },
+    {
+        survey: 'js',
+        year: 2017,
+        source: 'typeform',
+        typeformId: 'S5iLk9',
+        sections: {
+            [sections.JAVASCRIPT_FLAVORS]: {
+                title: 'JavaScript Flavor'
+            },
+            [sections.FRONTEND_FRAMEWORKS]: {
+                title: 'Front-End',
+                freeform: 'Other Front-End Frameworks'
+            },
+            [sections.DATA_LAYER]: {
+                title: 'Data Layer',
+                freeform: 'Other Data Management Solutions'
+            },
+            [sections.BACKEND_FRAMEWORKS]: {
+                title: 'Back-End',
+                freeform: 'Other Back-End Tools'
+            },
+            [sections.TESTING]: {
+                title: 'Testing',
+                freeform: 'Other testing frameworks'
+            },
+            [sections.CSS]: {
+                title: 'CSS',
+                freeform: 'Other CSS solutions'
+            },
+            [sections.BUILD_TOOLS]: {
+                title: 'Build Tools',
+                freeform: 'Other build tools'
+            },
+            [sections.MOBILE_DESKTOP]: {
+                title: 'Mobile & Desktop',
+                freeform: 'Other mobile/desktop apps solutions'
+            }
+        },
+        experience: {
+            [`I've USED it before, and WOULD use it again`]: experience.WOULD_USE,
+            [`I've USED it before, and would NOT use it again`]: experience.WOULD_NOT_USE,
+            [`I've HEARD of it, and WOULD like to learn it`]: experience.INTERESTED,
+            [`I've HEARD of it, and am NOT interested`]: experience.NOT_INTERESTED,
+            [`I've never heard of it`]: experience.NEVER_HEARD
+        }
+    },
+    {
+        survey: 'js',
+        year: 2018,
+        source: 'typeform',
+        typeformId: 'J9gRJf',
+        sections: {
+            [sections.JAVASCRIPT_FLAVORS]: {
+                title: 'JavaScript Flavors',
+                freeform: 'Other JavaScript Flavors options'
+            },
+            [sections.FRONTEND_FRAMEWORKS]: {
+                title: 'Front-End',
+                freeform: 'Other Front-End options'
+            },
+            [sections.DATA_LAYER]: {
+                title: 'Data Layer',
+                freeform: 'Other Data Layer options'
+            },
+            [sections.BACKEND_FRAMEWORKS]: {
+                title: 'Back-end',
+                freeform: 'Other Back-end options'
+            },
+            [sections.TESTING]: {
+                title: 'Testing',
+                freeform: 'Other Testing options'
+            },
+            [sections.MOBILE_DESKTOP]: {
+                title: 'Mobile & Desktop',
+                freeform: 'Other Mobile & Desktop options'
+            }
+        },
+        experience: {
+            [`👍 Used it > Would use again`]: experience.WOULD_USE,
+            [`👎 Used it > Would avoid`]: experience.WOULD_NOT_USE,
+            [`✅ Heard of it > Would like to learn`]: experience.INTERESTED,
+            [`🚫 Heard of it > Not interested`]: experience.NOT_INTERESTED,
+            [`🤷 Never heard of it/Not sure what it is`]: experience.NEVER_HEARD
+        }
+    },
+    {
+        survey: 'js',
+        year: 2019,
+        source: 'mongo',
+        mapping: {
+            _id: '!ignore',
+            surveyId: '!ignore',
+            userId: '!ignore',
+            year: '=',
+            completion: '=',
+            device: 'user_info.device',
+            referrer: 'user_info.referrer',
+            os: '=',
+            browser: '=',
+            version: '=',
+            source: '=',
+            createdAt: '=',
+            updatedAt: '=',
+
+            syntax_arrowfunctions: 'syntax.arrow_functions',
+            syntax_destructuring: 'syntax.destructuring',
+            syntax_spreadoperator: 'syntax.spread_operator',
+
+            language_asyncawait: 'language.async_await',
+            language_decorators: 'language.decorators',
+            language_promises: 'language.promises',
+            language_proxies: 'language.proxies',
+
+            datastructures_arrayprototypeflat: 'data_structures.array_prototype_flat',
+            datastructures_maps: 'data_structures.maps',
+            datastructures_sets: 'data_structures.sets',
+            datastructures_typedarrays: 'data_structures.typed_arrays',
+
+            browserapis_fetchapi: 'browser_apis.fetch',
+            browserapis_i18n: 'browser_apis.i18n',
+            browserapis_localstorage: 'browser_apis.local_storage',
+            browserapis_serviceworkers: 'browser_apis.service_workers',
+            browserapis_webanimationsapi: 'browser_apis.web_animations',
+            browserapis_webaudioapi: 'browser_apis.web_audio',
+            browserapis_webcomponents: 'browser_apis.web_components',
+            browserapis_webgl: 'browser_apis.webgl',
+            browserapis_webrtc: 'browser_apis.webrtc',
+            browserapis_websocket: 'browser_apis.websocket',
+            browserapis_webspeechapi: 'browser_apis.web_speech',
+            browserapis_webvr: 'browser_apis.webvr',
+
+            otherfeatures_progressivewebappspwa: 'other_features.pwa',
+            otherfeatures_webassemblywasm: 'other_features.wasm',
+
+            patterns_functionalprogramming: 'patterns.functional_programming',
+            patterns_objectorientedprogramming: 'patterns.object_oriented_programming',
+            patterns_reactiveprogramming: 'patterns.reactive_programming',
+
+            javascriptflavors_clojurescript: 'tools.clojurescript.experience',
+            javascriptflavors_elm: 'tools.elm.experience',
+            javascriptflavors_purescript: 'tools.purescript.experience',
+            javascriptflavors_reason: 'tools.reason.experience',
+            javascriptflavors_typescript: 'tools.typescript.experience',
+            javascriptflavors_overallhappiness: `happiness.${sections.JAVASCRIPT_FLAVORS}`,
+            javascriptflavors_otherjavascriptflavors: `sections_other_tools.${sections.JAVASCRIPT_FLAVORS}`,
+
+            frontendframeworks_angular: 'tools.angular.experience',
+            frontendframeworks_ember: 'tools.ember.experience',
+            frontendframeworks_preact: 'tools.preact.experience',
+            frontendframeworks_react: 'tools.react.experience',
+            frontendframeworks_svelte: 'tools.svelte.experience',
+            frontendframeworks_vue: 'tools.vuejs.experience',
+            frontendframeworks_overallhappiness: `happiness.${sections.FRONTEND_FRAMEWORKS}`,
+            frontendframeworks_otherfrontendframeworks: `sections_other_tools.${sections.FRONTEND_FRAMEWORKS}`,
+
+            datalayer_apollo: 'tools.apollo.experience',
+            datalayer_graphql: 'tools.graphql.experience',
+            datalayer_mobx: 'tools.mobx.experience',
+            datalayer_redux: 'tools.redux.experience',
+            datalayer_relay: 'tools.relay.experience',
+            datalayer_overallhappiness: `happiness.${sections.DATA_LAYER}`,
+            datalayer_otherdatalayertools: `sections_other_tools.${sections.DATA_LAYER}`,
+
+            backendframeworks_express: 'tools.express.experience',
+            backendframeworks_feathersjs: 'tools.feathers.experience',
+            backendframeworks_gatsbyjs: 'tools.gatsbyjs.experience',
+            backendframeworks_koa: 'tools.koa.experience',
+            backendframeworks_meteor: 'tools.meteor.experience',
+            backendframeworks_nextjs: 'tools.nextjs.experience',
+            backendframeworks_nuxtjs: 'tools.nuxtjs.experience',
+            backendframeworks_sails: 'tools.sails.experience',
+            backendframeworks_overallhappiness: `happiness.${sections.BACKEND_FRAMEWORKS}`,
+            backendframeworks_otherbackendframeworks: `sections_other_tools.${sections.BACKEND_FRAMEWORKS}`,
+
+            testing_ava: 'tools.ava.experience',
+            testing_cypress: 'tools.cypress.experience',
+            testing_enzyme: 'tools.enzyme.experience',
+            testing_jasmine: 'tools.jasmine.experience',
+            testing_jest: 'tools.jest.experience',
+            testing_mocha: 'tools.mocha.experience',
+            testing_puppeteer: 'tools.puppeteer.experience',
+            testing_storybook: 'tools.storybook.experience',
+            testing_overallhappiness: `happiness.${sections.TESTING}`,
+            testing_othertestingtools: `sections_other_tools.${sections.TESTING}`,
+
+            mobiledesktop_cordova: 'tools.cordova.experience',
+            mobiledesktop_electron: 'tools.electron.experience',
+            mobiledesktop_expo: 'tools.expo.experience',
+            mobiledesktop_ionic: 'tools.ionic.experience',
+            mobiledesktop_nativeapps: 'tools.nativeapps.experience',
+            mobiledesktop_nwjs: 'tools.nwjs.experience',
+            mobiledesktop_reactnative: 'tools.reactnative.experience',
+            mobiledesktop_overallhappiness: `happiness.${sections.MOBILE_DESKTOP}`,
+            mobiledesktop_othermobiledesktoptools: `sections_other_tools.${sections.MOBILE_DESKTOP}`,
+
+            othertools_browsers: 'other_tools.browsers.choices',
+            othertools_otherbrowsers: 'other_tools.browsers.others',
+            othertools_buildtools: 'other_tools.build_tools.choices',
+            othertools_otherbuildtools: 'other_tools.build_tools.others',
+            othertools_nonjslanguages: 'other_tools.non_js_languages.choices',
+            othertools_otherlanguages: 'other_tools.non_js_languages.others',
+            othertools_texteditors: 'other_tools.text_editors.choices',
+            othertools_othereditors: 'other_tools.text_editors.others',
+            othertools_utilities: 'other_tools.utilities.choices',
+            othertools_otherutilities: 'other_tools.utilities.others',
+
+            resources_blogsnewsmagazines: 'resources.blogs_news_magazines.choices',
+            resources_otherblogs: 'resources.blogs_news_magazines.others',
+            resources_podcasts: 'resources.podcasts.choices',
+            resources_otherpodcasts: 'resources.podcasts.others',
+            resources_sitescourses: 'resources.sites_courses.choices',
+            resources_othercourses: 'resources.sites_courses.others',
+
+            opinionquestions_buildingjavascriptappsisoverlycomplexrightnow: 'opinions.building_js_apps_overly_complex',
+            opinionquestions_ienjoybuildingjavascriptapps: 'opinions.enjoy_building_js_apps',
+            opinionquestions_iwouldlikejavascripttobemymainprogramminglanguage: 'opinions.would_like_js_to_be_main_lang',
+            opinionquestions_javascriptismovingintherightdirection: 'opinions.js_moving_in_right_direction',
+            opinionquestions_javascriptisoverusedonline: 'opinions.js_over_used_online',
+            opinionquestions_thejavascriptecosystemischangingtoofast: 'opinions.js_ecosystem_changing_to_fast',
+            opinionquestions_whatdoyoufeeliscurrentlymissingfromjavascript: 'opinions.missing_from_js',
+
+            aboutyou_youremail: 'user_info.email',
+            aboutyou_backendproficiency: 'user_info.backend_proficiency',
+            aboutyou_cssproficiency: 'user_info.css_proficiency',
+            aboutyou_companysize: 'user_info.company_size',
+            aboutyou_jobtitle: 'user_info.job_title',
+            aboutyou_otherjobtitle: 'user_info.job_title',
+            aboutyou_yearlysalary: 'user_info.yearly_salary',
+            aboutyou_yearsofexperience: 'user_info.years_of_experience',
+            aboutyou_yourcountry: 'user_info.country',
+            aboutyou_yourgender: 'user_info.gender',
+            aboutyou_othergender: 'user_info.gender',
+            aboutyou_howdidyoufindoutaboutthissurvey: 'user_info.how_did_user_find_out_about_the_survey',
+        },
+        experience: {
+            would_use_again: experience.WOULD_USE,
+            would_not_use_again: experience.WOULD_NOT_USE,
+            interested: experience.INTERESTED,
+            not_interested: experience.NOT_INTERESTED,
+            neverheard: experience.NEVER_HEARD
+        }
+    },
+    {
+        survey: 'css',
+        year: 2019,
+        source: 'typeform',
+        typeformId: 'TxDuh6',
+    },
+]
